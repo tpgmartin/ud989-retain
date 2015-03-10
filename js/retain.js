8f848c0c97cad22c85ea4c898691d29186481473
Tom Martin <tom@tpgmartin.com>
2015-03-10 13:58:03 +0000

    Store datetime info

--- js/retain.js
+++ js/retain.js
@@ -20,7 +20,8 @@ $(function(){
     var octopus = {
         addNewNote: function(noteStr) {
             model.add({
-                content: noteStr
+                content: noteStr,
+                date: new Date()
             });
             view.render();
         },
@@ -52,7 +53,8 @@ $(function(){
             var htmlStr = '';
             octopus.getNotes().forEach(function(note){
                 htmlStr += '<li class="note">'+
-                        note.content +
+                        note.content + ' - ' +
+                        note.date + 
                     '</li>';
             });
             this.noteList.html( htmlStr );
